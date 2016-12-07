072e5c5e2b96d36acc39ffc5f014b1b2cad48a77
Ryan_Thomas <rthoma11@villanova.edu>
2016-12-07 11:32:11 -0800

    classwork edits

--- sample-code-jsForEach/js/app.js
+++ sample-code-jsForEach/js/app.js
@@ -4,11 +4,24 @@ $(document).ready(function(){
 
   // select a template script, and pull out the handlebars and html inside it
   var source = $('#developer-li-template').html();
-  console.log('template script source:', source);
+  //console.log('template script source:', source);
+
+  var header_source = $("#header-template").html();
+  console.log("header template script source: ", header_source);
 
   // compile the handlebars template
   var template = Handlebars.compile(source);
-  console.log(template);
+  //console.log(template);
+
+  var header_template = Handlebars.compile(header_source);
+    console.log("header source", header_source);
+
+  var headerHtml = header_template({
+    cohortNumber: data.cohort,
+    roomNumber: data.room,
+    scheduleUrl: data.github_schedule
+  });
+  console.log(headerHtml);
 
   // use the template function from handlebars to create an HTML string
   // the template function takes in an object where:
@@ -23,5 +36,23 @@ $(document).ready(function(){
 
     // append html to the view
     $("#developers-list").append(developerHtml);
+
   });
+    $('header').append(headerHtml);
 });
+
+
+
+//  <header>
+  //   <script id="header-template" type="text/x-handlebars-template">
+  //   <h1>WDI {{cohortNumber}} Rocks!</h1>
+  //   <div class="where row">
+  //     <div class="col-md-6">
+  //       <p>room {{roomNumber}}</p>
+  //     </div>
+  //     <div class="col-md-6">
+  //       <p>online: <a href="https://github.com/{{sheduleUrl}}" target="_blank"></a></p>
+  //     </div>
+  //   </div>
+  // </script>
+  // </header>
