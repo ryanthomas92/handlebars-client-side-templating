7ea1b51043c0e034915032981aefdbdf281dd868
Brianna Veenstra <bgveenstra@users.noreply.github.com>
2016-12-07 09:29:11 -0800

    Update data.js

--- sample-code-jsForEach/js/data.js
+++ sample-code-jsForEach/js/data.js
@@ -2,6 +2,93 @@ var data = {
   school: "General Assembly",
   city: "San Francisco",
   course: "Web Development Immersive",
-  course_abbreviation: "WDI"
-  // add your data here
+  course_abbreviation: "WDI",
+  cohort: 34,
+  room: 7,
+  github_schedule: "https://github.com/sf-wdi-34/schedule",
+  developers:[
+    {
+      first_name: 'Alessandro',
+      last_name: 'Pianetta',
+      github_username: 'aapiane09'
+    },
+    {
+      last_name: "Dean",
+      first_name: "LD",
+      github_username: "vedelopment"
+    },
+    {
+      last_name: "Ricardo",
+      first_name: "Arellano",
+      github_username: "ricarellano"
+    },
+    {
+      first_name: 'The', 
+      last_name: 'Octocat',
+      github_username: 'octocat'    
+    },
+    {
+      first_name: 'Ryan',
+      last_name: 'Johnson',
+      status: 'hella-smart',
+      github_username: 'johsnon-rl'
+    },
+    {
+      first_name: 'Aaron',
+      last_name: "Steele",
+      github_username: 'nzoLogic'
+    },
+    {
+      first_name: "Ryan", 
+      last_name: "Thomas",
+      github_username: "ryanthomas92"
+    },
+    {
+      first_name: "Emmanuel",
+      last_name: "Kanu",
+      github_username: "Anichek25"
+    },
+    {
+      first_name: 'Amber',
+      last_name: 'Quoss',
+      github_username: 'aquoss'
+    },
+    {
+      first_name: "Shivinder",
+      last_name: "Gogna",
+      status: "single and ready to mingle",
+      github_username: "shivngiggles",
+      current_job: 'magician in training'
+    },
+    {
+      first_name: "Michael",
+      last_name: "Blair",
+      github_username: "mblair415",
+      motorcycle: "Honda Ruckus"
+    }, 
+    {
+      last_name: "Cusimano",
+      first_name: "Zach",
+      github_username: "c00z"
+    },           
+    {
+      last_name: "Silva",
+      first_name: "Greice",
+      github_username: "greicens"
+    }          
+  
+  ],
+  instructional_team: [
+    {
+      last_name: "Fauver",
+      first_name: "Cory",
+      github_username: "cofauver"
+    },
+    {
+      last_name: "Veenstra",
+      first_name: "Brianna",
+      github_username: "bgveenstra"
+    }
+  ]
 };
+
